25bae41c7bff1eeefd9a8b96d1599f8aae25ce1e
Arnab Mandal <153118360+ArnabMandal2857@users.noreply.github.com>
2024-03-01 01:37:41 +0530

    Add files via upload

---
+++ languages.js
@@ -0,0 +1,31 @@
+const languages = [
+  {
+    no: "1",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+  {no:"2",
+  name: " Bengali (বাংলা)",
+  native: "বাংলা",
+  code: "bn-IN"
+  },
+  {no:"3",
+  name:"Punjabi (ਪੰਜਾਬੀ)",
+  native: "ਪੰਜਾਬੀ",
+  code: "pa-IN"
+  },
+  {
+    no: "4",
+    name: "Hindi (हिन्दी)",
+    native: "हिन्दी",
+    code: "hi",
+  },
+  
+  {
+    no: "5",
+    name: "Urdu (اردو)",
+    native: "اردو",
+    code: "ur",
+  },
+];
